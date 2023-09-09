f8172b7bc98b6d068bca0bd494000765940545cb
unknown <johnatanpaes@gmail.com>
2023-09-09 01:57:13 -0300

    muita dificuldade para rodar o TypeScript, não consegui seguir o slide

---
+++ aula1/teste.js
@@ -0,0 +1,6 @@
+console.log("Olá Mundo, olha eu de novo");
+
+var nome = "John";
+let idade = 30;
+
+console.log(nome, 'sua idade é ', idade);
---
+++ aula1/teste2.ts
@@ -0,0 +1,6 @@
+console.log(`Olá mundo, olha eu de novo!`)
+
+let nomets = "John";
+let sobrenome = "Rasta Paes ";
+
+console.log(nomets, sobrenome)
